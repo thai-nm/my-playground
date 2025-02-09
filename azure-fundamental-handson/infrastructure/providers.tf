164f8a75ce4d15e013a0cdcda1baff28ecd8b4c9
Thai Nguyen <thainm.uet@gmail.com>
2025-02-09 22:47:46 +0700

    Update remote backend

--- azure-fundamental-handson/infrastructure/providers.tf
+++ azure-fundamental-handson/infrastructure/providers.tf
@@ -10,6 +10,7 @@ terraform {
       storage_account_name = "thainmtfstate"
       container_name       = "tfstate"
       key                  = "terraform.tfstate"
+      use_oidc             = true
   }
 }
 
