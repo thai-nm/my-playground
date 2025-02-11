d799428243b691c93f8a9efd4e5a84fdc971d0f1
Thai Nguyen <thainm.uet@gmail.com>
2025-02-12 04:41:09 +0700

    Add Cloudflare provider

--- azure-fundamental-handson/infrastructure/main.tf
+++ azure-fundamental-handson/infrastructure/main.tf
@@ -1,5 +1,6 @@
 locals {
   project = "afh"
+  cloudflare_zone_id = "ae3afeae2c316d39789d08c3bf022c28"
 }
 
 
--- azure-fundamental-handson/infrastructure/providers.tf
+++ azure-fundamental-handson/infrastructure/providers.tf
@@ -4,16 +4,22 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.15.0"
     }
+
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "5.0.0"
+    }
   }
   backend "azurerm" {
     resource_group_name  = "misc"
     storage_account_name = "thainmtfstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
 }
+
+provider "cloudflare" {}
