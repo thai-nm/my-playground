1da7cc09fbbdd46cbd594407af0b9f87bbb7cede
Thai Nguyen <thainm.uet@gmail.com>
2025-02-16 20:18:08 +0700

    Add compute resources

--- azure-fundamental-handson/infrastructure/network.tf
+++ azure-fundamental-handson/infrastructure/network.tf
@@ -58,6 +58,6 @@ resource "azurerm_lb" "public" {
 
   frontend_ip_configuration {
     name                 = "FeedbackAppPublicIP"
-    public_ip_address_id = azurerm_public_ip.feedback_app.id
+    public_ip_address_id = azurerm_public_ip.feedback-app.id
   }
 }
