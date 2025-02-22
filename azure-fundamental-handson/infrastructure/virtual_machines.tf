c4fac3264b7834f852c38f21e408f79d8c26c6ff
Thai Nguyen <thainm.uet@gmail.com>
2025-02-22 17:50:18 +0700

    Update OS disk type

--- azure-fundamental-handson/infrastructure/virtual_machines.tf
+++ azure-fundamental-handson/infrastructure/virtual_machines.tf
@@ -36,7 +36,7 @@ resource "azurerm_linux_virtual_machine" "feedback-app" {
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "StandardSSD_LRS"
+    storage_account_type = "Premium_LRS"
   }
 
   source_image_reference {
