e6599dcce08e2fc2b6570a7b29fa2fc9264218c9
Thai Nguyen <thainm.uet@gmail.com>
2025-02-22 19:58:03 +0700

    Update Flexible Server for PostgreSQL

--- azure-fundamental-handson/infrastructure/databases.tf
+++ azure-fundamental-handson/infrastructure/databases.tf
@@ -1,21 +1,21 @@
 
 
 resource "azurerm_postgresql_flexible_server" "example" {
-  name                          = "example-psqlflexibleserver"
-  resource_group_name           = azurerm_resource_group.example.name
-  location                      = azurerm_resource_group.example.location
-  version                       = "12"
-  delegated_subnet_id           = azurerm_subnet.example.id
-  private_dns_zone_id           = azurerm_private_dns_zone.example.id
+  name                          = "afh-postgresql"
+  resource_group_name           = azurerm_resource_group.afh.name
+  location                      = azurerm_resource_group.afh.location
+  version                       = "16"
+  delegated_subnet_id           = azurerm_subnet.delegated-flexible-server-postgresql
+  private_dns_zone_id           = azurerm_private_dns_zone.flexible-server-postgresql.id
   public_network_access_enabled = false
-  administrator_login           = "psqladmin"
-  administrator_password        = "H@Sh1CoR3!"
+  administrator_login           = "dbadmin"
+  administrator_password        = "dbadmin@25879"
   zone                          = "1"
 
   storage_mb   = 32768
   storage_tier = "P4"
 
   sku_name   = "B_Standard_B1ms"
-  depends_on = [azurerm_private_dns_zone_virtual_network_link.example]
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.flexible-server-postgresql]
 
 }
