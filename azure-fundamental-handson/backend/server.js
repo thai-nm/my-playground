9fb16e31e9eea95c1a169d9442f2ee15e4c6eab2
Thai Nguyen <thainm.uet@gmail.com>
2025-02-22 19:06:49 +0700

    Add backend Dockerfile

--- azure-fundamental-handson/backend/server.js
+++ azure-fundamental-handson/backend/server.js
@@ -36,7 +36,7 @@ app.get("/", (req, res) => {
   });
 
 // Start the server
-const PORT = 3000;
+const PORT = 80;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
