51c12b5c088a380e1535b136634622c6695ee1ef
Thai Nguyen <thainm.uet@gmail.com>
2025-04-04 04:09:36 +0700

    CNOK: Update Kong config with JWT plugin

--- cloud-native-on-kubernetes/services/user-service/routes/user_routes.go
+++ cloud-native-on-kubernetes/services/user-service/routes/user_routes.go
@@ -3,13 +3,14 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"user-service/controllers"
-	"user-service/middleware"
+	// "user-service/middleware"
 )
 
 func RegisterRoutes(router *gin.Engine) {
 	routes := router.Group("/user")
 	{
 		routes.POST("/register", controllers.RegisterUser)
-		routes.GET("/profile", middleware.AuthMiddleware(), controllers.GetProfile)
+		routes.GET("/profile", controllers.GetProfile)
+		// routes.GET("/profile", middleware.AuthMiddleware(), controllers.GetProfile)
 	}
 }
